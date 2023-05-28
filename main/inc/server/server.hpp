aec0dff564f40fed98a9136996a3a2cf5651ef61
Ajay-26 <aj.sailopal@gmail.com>
2023-05-27 21:15:38 -0500

    built boilerplate code for server. need to make code to prototype and to handle incoming clients

---
+++ main/inc/server/server.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cerrno>
+
+//not part of the standard so they never made cunistd.h.... So weird
+#include <unistd.h>
+
+//not sure what the C++ equivalent headers are
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <netdb.h>
+
+
+class Server{
+private:
+    char *port;
+    char *server_hostname;
+    int backlog;
+    int buffer_size; //use the same buffer size for all buffers
+    int passive_socket_fd; //file descriptor for passive socket which listens for connections 
+
+public:
+    // This function sets up the socket stuff and listens for incoming connections
+    void create_server(char* port_p, int buffer_size = 1024, int backlog = 10); 
+    
+    // This function will end the server and close all connections
+    void destroy_server(); 
+};
---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+#include "server.hpp"
+
+int main(int argc, char **argv){
+    std::printf("Hi\n");
+    Server s;
+    s.create_server("6667");
+}   
