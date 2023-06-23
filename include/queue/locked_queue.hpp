db84bfaca158f4b625253bc7f5c3ebfb39cbf0aa
Ajay-26 <aj.sailopal@gmail.com>
2023-06-23 10:37:27 -0500

    fixed template side code issues in locked queue

--- include/queue/locked_queue.hpp
+++ include/queue/locked_queue.hpp
@@ -23,6 +23,37 @@ private:
     std::priority_queue<T, std::vector<T>, Compare> heap_;
 };
 
+template<typename T, typename Compare>
+LockedPriorityQueue<T, Compare>::LockedPriorityQueue() {}
+
+template<typename T, typename Compare>
+void LockedPriorityQueue<T, Compare>::push(const T& value)
+{
+    std::unique_lock<std::shared_mutex> lock(mutex_);
+    heap_.push(value);
+}
+
+template<typename T, typename Compare>
+void LockedPriorityQueue<T, Compare>::pop()
+{
+    std::unique_lock<std::shared_mutex> lock(mutex_);
+    heap_.pop();
+}
+
+template<typename T, typename Compare>
+T LockedPriorityQueue<T, Compare>::top() const
+{
+    std::shared_lock<std::shared_mutex> lock(mutex_);
+    return heap_.top();
+}
+
+template<typename T, typename Compare>
+bool LockedPriorityQueue<T, Compare>::empty() const
+{
+    std::unique_lock<std::shared_mutex> lock(mutex_);
+    return heap_.empty();
+}
+
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #include "doctest/doctest.h"
 TEST_CASE("test the locked queue int template")
